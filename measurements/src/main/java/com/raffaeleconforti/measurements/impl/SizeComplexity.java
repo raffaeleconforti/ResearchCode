fb0eeeceed38ad337feea0a92681e3710b7ed1e3
nemo-91 <adrn.agst@gmail.com>
2016-10-19 14:32:08 +0300

    added new classes for complexity metrics

---
+++ measurements/src/main/java/com/raffaeleconforti/measurements/impl/SizeComplexity.java
@@ -0,0 +1,32 @@
+package com.raffaeleconforti.measurements.impl;
+
+import au.edu.qut.metrics.ComplexityCalculator;
+import com.raffaeleconforti.conversion.petrinet.PetriNetToBPMNConverter;
+import com.raffaeleconforti.measurements.MeasurementAlgorithm;
+import com.raffaeleconforti.wrapper.MiningAlgorithm;
+import com.raffaeleconforti.wrapper.PetrinetWithMarking;
+import org.deckfour.xes.classification.XEventClassifier;
+import org.deckfour.xes.model.XLog;
+import org.processmining.contexts.uitopia.UIPluginContext;
+import org.processmining.models.graphbased.directed.bpmn.BPMNDiagram;
+
+/**
+ * Created by Adriano on 19/10/2016.
+ */
+public class SizeComplexity  implements MeasurementAlgorithm {
+
+    @Override
+    public double computeMeasurement(UIPluginContext pluginContext, XEventClassifier xEventClassifier, PetrinetWithMarking petrinetWithMarking, MiningAlgorithm miningAlgorithm, XLog log) {
+        try {
+            BPMNDiagram bpmn = PetriNetToBPMNConverter.convert(petrinetWithMarking.getPetrinet(), petrinetWithMarking.getInitialMarking(), petrinetWithMarking.getFinalMarking(), false);
+            ComplexityCalculator cc = new ComplexityCalculator(bpmn);
+            String size = cc.computeSize();
+            return Double.valueOf(size);
+        } catch( Exception e ) { return -1; }
+    }
+
+    @Override
+    public String getMeasurementName() {
+        return "Size";
+    }
+}
---
+++ measurements/src/main/java/com/raffaeleconforti/measurements/impl/StructurednessComplexity.java
@@ -0,0 +1,32 @@
+package com.raffaeleconforti.measurements.impl;
+
+import au.edu.qut.metrics.ComplexityCalculator;
+import com.raffaeleconforti.conversion.petrinet.PetriNetToBPMNConverter;
+import com.raffaeleconforti.measurements.MeasurementAlgorithm;
+import com.raffaeleconforti.wrapper.MiningAlgorithm;
+import com.raffaeleconforti.wrapper.PetrinetWithMarking;
+import org.deckfour.xes.classification.XEventClassifier;
+import org.deckfour.xes.model.XLog;
+import org.processmining.contexts.uitopia.UIPluginContext;
+import org.processmining.models.graphbased.directed.bpmn.BPMNDiagram;
+
+/**
+ * Created by Adriano on 19/10/2016.
+ */
+public class StructurednessComplexity implements MeasurementAlgorithm {
+
+    @Override
+    public double computeMeasurement(UIPluginContext pluginContext, XEventClassifier xEventClassifier, PetrinetWithMarking petrinetWithMarking, MiningAlgorithm miningAlgorithm, XLog log) {
+        try {
+            BPMNDiagram bpmn = PetriNetToBPMNConverter.convert(petrinetWithMarking.getPetrinet(), petrinetWithMarking.getInitialMarking(), petrinetWithMarking.getFinalMarking(), false);
+            ComplexityCalculator cc = new ComplexityCalculator(bpmn);
+            String structuredness = cc.computeStructuredness();
+            return Double.valueOf(structuredness);
+        } catch (Exception e) { return -1; }
+    }
+
+    @Override
+    public String getMeasurementName() {
+        return "Structuredness";
+    }
+}
