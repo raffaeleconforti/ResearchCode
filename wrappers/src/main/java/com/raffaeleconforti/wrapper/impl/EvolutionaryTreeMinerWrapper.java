55a2fa9c3b850feeb155046a77fe80ffcee12c16
raffaeleconforti <conforti.raffaele@gmail.com>
2016-10-28 15:15:20 +1000

    Fixed mining algorithms for Benchmark

--- wrappers/src/main/java/com/raffaeleconforti/wrapper/impl/EvolutionaryTreeMinerWrapper.java
+++ wrappers/src/main/java/com/raffaeleconforti/wrapper/impl/EvolutionaryTreeMinerWrapper.java
@@ -1,18 +1,33 @@
 package com.raffaeleconforti.wrapper.impl;
 
+import com.raffaeleconforti.context.FakePluginContext;
 import com.raffaeleconforti.conversion.petrinet.PetriNetToBPMNConverter;
 import com.raffaeleconforti.wrapper.LogPreprocessing;
 import com.raffaeleconforti.wrapper.MiningAlgorithm;
 import com.raffaeleconforti.wrapper.PetrinetWithMarking;
+import com.raffaeleconforti.wrapper.marking.MarkingDiscoverer;
 import org.deckfour.xes.model.XLog;
 import org.processmining.contexts.uitopia.UIPluginContext;
 import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
 import org.processmining.framework.plugin.annotations.Plugin;
 import org.processmining.framework.plugin.annotations.PluginVariant;
 import org.processmining.models.graphbased.directed.bpmn.BPMNDiagram;
+import org.processmining.models.graphbased.directed.petrinet.Petrinet;
+import org.processmining.models.semantics.petrinet.Marking;
+import org.processmining.plugins.etm.ETM;
+import org.processmining.plugins.etm.model.narytree.NAryTree;
+import org.processmining.plugins.etm.model.narytree.TreeUtils;
+import org.processmining.plugins.etm.model.narytree.conversion.NAryTreeToProcessTree;
+import org.processmining.plugins.etm.parameters.ETMParam;
+import org.processmining.plugins.etm.parameters.ETMParamFactory;
 import org.processmining.plugins.etm.ui.plugins.ETMPlugin;
 import org.processmining.processtree.ProcessTree;
 import org.processmining.processtree.conversion.ProcessTree2Petrinet;
+import org.uncommonseditedbyjoosbuijs.watchmaker.framework.TerminationCondition;
+
+import java.io.*;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by conforti on 9/02/2016.
@@ -37,12 +52,30 @@ public class EvolutionaryTreeMinerWrapper implements MiningAlgorithm {
             LogPreprocessing logPreprocessing = new LogPreprocessing();
             log = logPreprocessing.preprocessLog(context, log);
 
-            ETMPlugin etmPlugin = new ETMPlugin();
-            ProcessTree processTree = etmPlugin.withoutSeed(context, log);
+            System.setOut(new PrintStream(new OutputStream() {
+                @Override
+                public void write(int b) throws IOException {}
+            }));
+
+            ProcessTree processTree;
+            if(context instanceof FakePluginContext) {
+                ETMParam params = ETMParamFactory.buildStandardParamConfigurable(context, new XLog[] {log});
+                ETM etm = new ETM(params);
+                etm.run();
+                NAryTree tree1 = etm.getResult();
+                processTree = NAryTreeToProcessTree.convert(params.getCentralRegistry().getEventClasses(), tree1, "Process tree discovered by the ETM algorithm");
+            }else {
+                ETMPlugin etmPlugin = new ETMPlugin();
+                processTree = etmPlugin.withoutSeed(context, log);
+            }
             ProcessTree2Petrinet.PetrinetWithMarkings petrinetWithMarkings = ProcessTree2Petrinet.convert(processTree);
 
             logPreprocessing.removedAddedElements(petrinetWithMarkings.petrinet);
 
+            System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+
+            MarkingDiscoverer.createInitialMarkingConnection(context, petrinetWithMarkings.petrinet, petrinetWithMarkings.initialMarking);
+            MarkingDiscoverer.createInitialMarkingConnection(context, petrinetWithMarkings.petrinet, petrinetWithMarkings.finalMarking);
             return new PetrinetWithMarking(petrinetWithMarkings.petrinet, petrinetWithMarkings.initialMarking, petrinetWithMarkings.finalMarking);
         } catch (ProcessTree2Petrinet.InvalidProcessTreeException e) {
             e.printStackTrace();
