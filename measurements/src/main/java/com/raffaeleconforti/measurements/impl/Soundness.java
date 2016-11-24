7058aeb3ac3b62e2aa0c134a0fc0edc596864253
nemo-91 <adrn.agst@gmail.com>
2016-11-24 15:34:44 +0200

    added soundness (not working though)

--- measurements/src/main/java/com/raffaeleconforti/measurements/impl/Soundness.java
+++ measurements/src/main/java/com/raffaeleconforti/measurements/impl/Soundness.java
@@ -1,7 +1,5 @@
 package com.raffaeleconforti.measurements.impl;
 
-import au.edu.qut.metrics.ComplexityCalculator;
-import com.raffaeleconforti.conversion.petrinet.PetriNetToBPMNConverter;
 import com.raffaeleconforti.measurements.Measure;
 import com.raffaeleconforti.measurements.MeasurementAlgorithm;
 import com.raffaeleconforti.wrapper.MiningAlgorithm;
@@ -9,8 +7,7 @@ import com.raffaeleconforti.wrapper.PetrinetWithMarking;
 import org.deckfour.xes.classification.XEventClassifier;
 import org.deckfour.xes.model.XLog;
 import org.processmining.contexts.uitopia.UIPluginContext;
-import org.processmining.models.graphbased.directed.petrinet.Petrinet;
-import org.processmining.models.semantics.petrinet.Marking;
+
 
 /**
  * Created by Adriano on 23/11/2016.
