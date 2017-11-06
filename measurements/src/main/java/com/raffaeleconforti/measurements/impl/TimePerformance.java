74bdf1f590d47fd46ff14e8936a2c9b1e0891ada
nemo-91 <adrn.agst@gmail.com>
2017-11-06 16:11:54 +0200

    added time performance metric

---
+++ measurements/src/main/java/com/raffaeleconforti/measurements/impl/TimePerformance.java
@@ -0,0 +1,59 @@
+package com.raffaeleconforti.measurements.impl;
+
+import au.edu.qut.petrinet.tools.SoundnessChecker;
+import com.raffaeleconforti.measurements.Measure;
+import com.raffaeleconforti.measurements.MeasurementAlgorithm;
+import com.raffaeleconforti.wrapper.MiningAlgorithm;
+import com.raffaeleconforti.wrapper.PetrinetWithMarking;
+import org.deckfour.xes.classification.XEventClassifier;
+import org.deckfour.xes.model.XLog;
+import org.processmining.contexts.uitopia.UIPluginContext;
+import org.processmining.models.graphbased.directed.petrinet.Petrinet;
+import org.processmining.processtree.ProcessTree;
+
+/**
+ * Created by Adriano on 13/10/2017.
+ */
+public class TimePerformance implements MeasurementAlgorithm {
+
+    static int REP = 5;
+
+    @Override
+    public Measure computeMeasurement(UIPluginContext pluginContext, XEventClassifier xEventClassifier, ProcessTree processTree, MiningAlgorithm miningAlgorithm, XLog log) {
+        return null;
+    }
+
+    @Override
+    public Measure computeMeasurement(UIPluginContext pluginContext, XEventClassifier xEventClassifier,
+                                      PetrinetWithMarking petrinetWithMarking, MiningAlgorithm miningAlgorithm, XLog log) {
+        Measure measure = new Measure();
+        double otime;
+        double etime;
+
+        try {
+            etime = 0;
+            for(int i = 0; i<REP; i++) {
+                otime = System.currentTimeMillis();
+                miningAlgorithm.mineBPMNDiagram(pluginContext, log, false, null, xEventClassifier);
+                etime += (System.currentTimeMillis() - otime);
+            }
+            etime = etime/REP;
+            measure.addMeasure(getAcronym(), etime);
+        } catch( Exception e ) { return measure; }
+
+        return  measure;
+    }
+
+    @Override
+    public String getMeasurementName() {
+        return "Time Performance";
+    }
+
+    @Override
+    public String getAcronym() {return "avg-time";}
+
+    @Override
+    public boolean isMultimetrics() {
+        return true;
+    }
+}
